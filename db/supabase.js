1d63b8ae6e0166f7b65381a66bcde0e08c053e13
myspidr1511-max <myspidr1511@gmail.com>
2025-09-01 21:15:55 +0300

    Delete db directory

--- db/supabase.js
+++
@@ -1,7 +0,0 @@
-// /src/db/supabase.js
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE || process.env.SUPABASE_ANON_KEY
-);
-module.exports = { supabase };
